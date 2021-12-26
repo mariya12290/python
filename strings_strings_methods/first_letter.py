f423122656b3b1dceaced12f6ce41fd39701ebf0
mariya12290 <kumar.mariya12@gmail.com>
2021-12-26 12:50:55 +0100

    added book

---
+++ strings_strings_methods/first_letter.py
@@ -0,0 +1,4 @@
+prompt ="Tell me your password: "
+
+user_input = input(prompt)
+print(user_input[0].upper())
---
+++ strings_strings_methods/string_numbers.py
@@ -0,0 +1,4 @@
+user_num ="Enter a int to multiply by 2: "
+user_input = input(user_num)
+user_input = int(user_input)
+print(user_input*2)
